81d959011c1dec6239b62f3bcbab65e1a4d67371
stein-a <stein.alver@me.com>
2025-06-03 12:59:16 +0200

    Initial Commit

---
+++ SATestCloudAppStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SATestCloudAppStore
+//
+//  Created by Stein Alver on 03/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SATestCloudAppStore/SATestCloudAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SATestCloudAppStoreApp.swift
+//  SATestCloudAppStore
+//
+//  Created by Stein Alver on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SATestCloudAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
